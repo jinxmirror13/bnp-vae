626b3a50b51d38d9a5ca012d377e3903114a9c09
Kate Highnam <57360661+jinxmirror13@users.noreply.github.com>
2020-07-09 15:12:11 -0400

    attempted translation no 1

--- src/util.py
+++ src/util.py
@@ -7,9 +7,18 @@ import tensorflow as tf
 import sys
 import resource
 
+#########################
+#    Hyperparameters    #
+#########################
+
+# For Training and Model Fitting AE
 batch_size = args.batch_size
 LATENT_CODE_SIZE = args.latent_code_size
+
+# Input
 IMG_DIM = {'width': 224, 'height': 224, 'channels': 3}
+
+# Misc
 #BRANCHING_FACTOR = eval(sys.argv[3])
 #NUM_LEVELS = eval(sys.argv[4])
 #NUM_INTERNAL_EDGES = BRANCHING_FACTOR * ((BRANCHING_FACTOR ** (NUM_LEVELS-2) - 1) /
@@ -19,20 +28,29 @@ IMG_DIM = {'width': 224, 'height': 224, 'channels': 3}
 #NUM_INTERNAL_NODES = NUM_NODES - NUM_PATHS
 #NUM_EDGES = BRANCHING_FACTOR * ((BRANCHING_FACTOR ** (NUM_LEVELS-1) - 1) / 
 #                                (BRANCHING_FACTOR - 1))
-# ncrp hyperparameters
+
+#################
+
+# nCRRP hyperparameters
 ALPHA = np.zeros(shape=LATENT_CODE_SIZE)
-GAMMA = 10.0
+
+# alpha for the dirichlet process, different from ALPHA used for nodes (consistent with their paper)
+GAMMA = 10.0 
+
+# sigma n and d?
 #SIGMA_B = 100000.0
 #SIGMA_Z = 100000.0
 SIGMA_B = 1.0
 SIGMA_Z = 1.0
+
 SIGMA_B_sqrinv = 1.0 / (SIGMA_B ** 2)
 SIGMA_Z_sqrinv = 1.0 / (SIGMA_Z ** 2)
 STDEV_THR = args.stdev_thr
 
 
 def composeAll(*args):
-    """Util for multiple function composition
+    """
+    Util for multiple function composition
 
     i.e. composed = composeAll([f, g, h])
          composed(x) # == f(g(h(x)))
